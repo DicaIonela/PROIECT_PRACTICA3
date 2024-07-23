0281e7995a145cf8560363be7bd7c83f3921ed3b
ioneladd <164873125+ioneladd@users.noreply.github.com>
2024-07-23 06:23:49 -0700

    Add files via upload

---
+++ Proiect_practicaDI/InterfataUtilizator/InterfataGrafica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfataUtilizator
+{
+    public partial class InterfataGrafica : Form
+    {
+        public InterfataGrafica()
+        {
+            this.Text = "Interfață Grafică";
+            this.Width = 800;
+            this.Height = 600;
+            InitializeComponent();
+        }
+
+        
+    }
+}
